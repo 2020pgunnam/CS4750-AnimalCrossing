ce1a54ddc2a3345d14efe8f47d37063a6244661c
Emily Yao <emily.yao673@gmail.com>
2023-04-19 20:17:25 -0400

    Set up listings.php
    Fixed syntax so that listings.php works

--- listings.php
+++ listings.php
@@ -6,18 +6,19 @@ require("connect-db.php");
 require("animalcrossing-db.php");
 // include("friend-db.php")
 
-$listings = selectAllListings();
+// $listings = selectAllListings();
 // var_dump($friends)
-$listings  = null;
+$listings = selectAllListings();
+// var_dump($listings)
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST')
-{
-  if ((!empty($_POST['actionBtn'])) && ($_POST['actionBtn'] == "Select Listings"))
-  {
-    $listings() = selectAllListings();
-    var_dump($listings);
-  }
-}
+// if ($_SERVER['REQUEST_METHOD'] == 'POST')
+// {
+//   if ((!empty($_POST['actionBtn'])) && ($_POST['actionBtn'] == "Select Listings"))
+//   {
+//     $listings = selectAllListings();
+//     var_dump($listings);
+//   }
+// }
 
 ?>
 <!-- 1. create HTML5 doctype -->
@@ -31,7 +32,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
   <meta name="author" content="your name">
   <meta name="description" content="include some description about your page">
 
-  <title>Bootstrap example</title>
+  <title>Listings</title>
 
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <link rel="icon" type="image/png" href="http://www.cs.virginia.edu/~up3f/cs4750/images/db-icon.png" />
@@ -42,27 +43,27 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
 <body>
   <div class="container">
     <h1>Listings</h1>
-    <form name="mainForm" action="simpleform.php" method="get">
-    <div class="row mb-3 mx-3">
-      <input type="submit" class="btn btn-primary" name="actionBtn" value="Select Listings" title="Click To Show Listings" />
-    </div>
+    <!-- <form name="mainForm" action="listings.php" method="post">
+      <div class="row mb-3 mx-3">
+        <input type="submit" class="btn btn-primary" name="actionBtn" value="Select Listings" title="Click To Show Listings" />
+      </div>
 
-  </form>
-<div class="row justify-content-center">
-<table class="w3-table w3-bordered w3-card-4 center" style="width:70%">
-  <thead>
-  <tr style="background-color:#B0B0B0">
-    <th>Item ID</th>
-    <th>Item Selling Price</th>
-  </tr>
-  </thead>
-<?php foreach ($listings as $item): ?>
-  <tr>
-     <td><?php echo $item['itemID']; ?></td>
-     <td><?php echo $item['itemSellingPrice']; ?></td>
-  </tr>
-<?php endforeach; ?>
-</table>
-</div>
+    </form> -->
+  <div class="row justify-content-center">
+    <table class="w3-table w3-bordered w3-card-4 center" style="width:70%">
+      <thead>
+        <tr style="background-color:#B0B0B0">
+          <th>Item ID</th>
+          <th>Item Selling Price</th>
+        </tr>
+      </thead>
+      <?php foreach ($listings as $item): ?>
+        <tr>
+          <td><?php echo $item['itemID']; ?></td>
+          <td><?php echo $item['itemSellingPrice']; ?></td>
+        </tr>
+      <?php endforeach; ?>
+    </table>
+  </div>
 </body>
 </html>
