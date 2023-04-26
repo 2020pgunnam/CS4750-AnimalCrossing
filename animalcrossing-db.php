71c3ee44f0079374506a8d3567d9c98347621b99
Emily Yao <emily.yao673@gmail.com>
2023-04-26 18:47:42 -0400

    Add add, delete, update listing functions to animalcrossing-db.php

--- animalcrossing-db.php
+++ animalcrossing-db.php
@@ -33,9 +33,11 @@ function selectInventory($name) {
     // db
     global $db;
     // query
-    $query = "select * from User natural join Inventory natural join Items where userName=:(:name)";
+    $query = "select * from User natural join Inventory natural join Items where userName=:name";
     // prepare
     $statement = $db->prepare($query);
+
+    $statement->bindValue(':name', $name);
     // execute
     $statement->execute();
     // retrieve
@@ -83,39 +85,52 @@ function getListingPriceByUserItem($userID, $itemID)
     $statement->closeCursor();
     return $result;
 }
-//
-// function deleteFriend($friend_to_delete)
-// {
-//     global $db;
-//     $query = "delete from friends where name=:friend_to_delete";
-//     $statement = $db->prepare($query);
-//     $statement->bindValue(':friend_to_delete', $friend_to_delete);
-//     $statement->execute();
-//     $statement->closeCursor();
-// }
-//
-// function getFriendByName($name)
-// {
-//     global $db;
-//     $query = "select * from friends where name=:name";
-//     $statement = $db->prepare($query);
-//     $statement->bindValue(':name', $name);
-//     $statement->execute();
-//     $statement->closeCursor();
-//     return $result;
-// }
-//
-// function updateFriend($name, $major, $year)
-// {
-//     echo "in updateFriend";
-//     global $db;
-//     $query = "update friends set major=:major, year=:year, where name=:name";
-//     $statement = $db->query($query);
-//     $statement->bindValue(':name', $name);
-//     $statement->bindValue(':major', $major);
-//     $statement->bindValue(':year', $year);
-//     $statement->execute();
-//     $statement->closeCursor();
-// }
+function updateListing($itemID, $userID, $sellingPrice)
+{
+    global $db;
+    $query = "update Listings set itemSellingPrice=:sellingPrice where (itemID=:itemID and sellerID=:userID)";
+    $statement = $db->query($query);
+    $statement->bindValue(':sellingPrice', $sellingPrice);
+    $statement->bindValue(':itemID', $itemID);
+    $statement->bindValue(':userID', $userID);
+    $statement->execute();
+    $statement->closeCursor();
+}
+
+function addListing($itemID, $userID, $sellingPrice)
+{
+    global $db;
+    $query = "insert into Listings values (:sellerID, :itemID, :itemSellingPrice)";
+    $statement = $db->prepare($query);
+    $statement->bindValue(':sellerID', $userID);
+    $statement->bindValue(':itemID', $itemID);
+    $statement->bindValue(':itemSellingPrice', $sellingPrice);
+    $statement->execute();
+    $statement->closeCursor();
+    // When creating new listing, should update for item numListingsAvailable
+    $query = "update Items set numListingsAvailable=(numListingsAvailable+1) where itemID=:itemID";
+    $statement = $db->prepare($query);
+    $statement->bindValue(':itemID', $itemID);
+    $statement->execute();
+    $statement->closeCursor();
+}
+
+function deleteListing($itemID, $userID)
+{
+    global $db;
+    $query = "delete from Listings where (itemID=:itemID and sellerID=:userID)";
+    $statement = $db->prepare($query);
+    $statement->bindValue(':sellerID', $userID);
+    $statement->bindValue(':itemID', $itemID);
+    $statement->execute();
+    $statement->closeCursor();
+    echo 'deleted itemID';
+    // When deleting listing, should update for item numListingsAvailable
+    $query = "update Items set numListingsAvailable=(numListingsAvailable-1) where itemID=:itemID";
+    $statement = $db->prepare($query);
+    $statement->bindValue(':itemID', $itemID);
+    $statement->execute();
+    $statement->closeCursor();
+}
 
 ?>
