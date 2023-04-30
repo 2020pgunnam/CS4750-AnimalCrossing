08a0e8ad39cb2c4f9dda47bb88bf6a00a09763da
Edmund Chen <edchen7321@gmail.com>
2023-04-30 15:55:55 -0400

    push

--- logout.php
+++ logout.php
@@ -15,7 +15,7 @@ if (ini_get("session.use_cookies")) {
 }
 // Finally, destroy the session.
 session_destroy();
-header("Location: login.php");
+header("Location: index.php");
 exit;
 ?>
 <!-- // Run the project by following URL:
